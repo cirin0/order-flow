fcb92a465c0ccf597b0a204f15a94590f22e64e4
cirin0 <ivan.gruziv@gmail.com>
2025-02-07 22:38:14 +0200

    remove commented-out login and registration endpoints in AuthController

--- src/main/java/org/flow/orderflow/controller/api/AuthController.java
+++ src/main/java/org/flow/orderflow/controller/api/AuthController.java
@@ -1,13 +1,7 @@
 package org.flow.orderflow.controller.api;
 
 import lombok.RequiredArgsConstructor;
-import org.flow.orderflow.dto.user.UserDto;
-import org.flow.orderflow.dto.user.UserLoginDto;
-import org.flow.orderflow.dto.user.UserRegistrationDto;
 import org.flow.orderflow.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,15 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
   private final UserService userService;
 
-  @PostMapping("/login")
-  public ResponseEntity<UserDto> login(@RequestBody UserLoginDto dto) {
-    return ResponseEntity.ok(userService.loginUser(dto));
-  }
-
-  @PostMapping("/register")
-  public ResponseEntity<UserDto> register(@RequestBody UserRegistrationDto dto) {
-    return ResponseEntity.ok(userService.registerUser(dto));
-  }
+//  @PostMapping("/login")
+//  public ResponseEntity<UserDto> login(@RequestBody UserLoginDto dto) {
+//    return ResponseEntity.ok(userService.loginUser(dto));
+//  }
+//
+//  @PostMapping("/register")
+//  public ResponseEntity<UserDto> register(@RequestBody UserRegistrationDto dto) {
+//    return ResponseEntity.ok(userService.registerUser(dto));
+//  }
 
 //  @PostMapping("/logout")
 //  public ResponseEntity<Boolean> logout(@RequestBody UserDto dto) {
