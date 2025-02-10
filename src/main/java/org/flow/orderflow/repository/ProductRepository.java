645c11b82b50c70d3eeb53336f483420c58d35d9
Oleksandr <oleksandr.danyliuk.22@pnu.edu.ua>
2025-02-10 23:19:46 +0200

    видалив зайвий код 2.0

--- src/main/java/org/flow/orderflow/repository/ProductRepository.java
+++ src/main/java/org/flow/orderflow/repository/ProductRepository.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-  void deleteAllByProductId(@Param("productId") Long productId);
   Product findByName(String name);
 
   List<Product> findByCategoryId(Long categoryId);
