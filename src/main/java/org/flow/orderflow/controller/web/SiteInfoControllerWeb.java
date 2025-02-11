51f262f63883737002f67f428175cf6eaa91f903
Ivan Hrabivchuk <ivgrabivchuk2005@gmail.com>
2025-02-11 19:29:37 +0200

    add few prikoliv

---
+++ src/main/java/org/flow/orderflow/controller/web/SiteInfoControllerWeb.java
@@ -0,0 +1,30 @@
+package org.flow.orderflow.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SiteInfoControllerWeb {
+
+  @GetMapping("/about")
+  public String aboutUs() {
+    return "about-contact/about";
+  }
+
+  @GetMapping("/delivery")
+  public String contactUs() {
+    return "about-contact/contact";
+  }
+
+  @GetMapping("/privacy")
+  public String privacy() {
+    return "privacy-term/privacy";
+  }
+
+  @GetMapping("/terms")
+  public String terms() {
+    return "privacy-term/term";
+  }
+
+}
+
