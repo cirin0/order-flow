50269800b685108261c9247a4e878afe477ca509
cirin0 <ivan.gruziv@gmail.com>
2025-02-13 20:34:16 +0200

    update error handling and add custom error pages for 400, 404, and 500 statuses

--- src/main/java/org/flow/orderflow/exception/GlobalExceptionHandler.java
+++ src/main/java/org/flow/orderflow/exception/GlobalExceptionHandler.java
@@ -1,18 +1,28 @@
 package org.flow.orderflow.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
   @ExceptionHandler(NotFound.class)
-  public ResponseEntity<ErrorResponse> handleNotFound(NotFound e) {
-    ErrorResponse errorResponse = new ErrorResponse(
-      HttpStatus.NOT_FOUND,
-      e.getMessage()
-    );
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+  public Object handleNotFound(NotFound e, HttpServletRequest request, Model model) {
+    String accept = request.getHeader("Accept");
+
+    if (accept != null && accept.contains("application/json")) {
+      ErrorResponse errorResponse = new ErrorResponse(
+        HttpStatus.NOT_FOUND,
+        e.getMessage()
+      );
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    } else {
+      model.addAttribute("error", e.getMessage());
+      model.addAttribute("status", HttpStatus.NOT_FOUND);
+      return "error/404";
+    }
   }
 }
