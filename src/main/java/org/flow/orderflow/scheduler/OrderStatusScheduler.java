7aba59881bfafb9b1eb749ed140d70d3ae587baa
cirin0 <ivan.gruziv@gmail.com>
2025-02-13 23:39:03 +0200

    refactor repositories and mappers to remove unused methods and improve order status scheduling

--- src/main/java/org/flow/orderflow/scheduler/OrderStatusScheduler.java
+++ src/main/java/org/flow/orderflow/scheduler/OrderStatusScheduler.java
@@ -12,26 +12,62 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrderStatusScheduler {
   private final OrderRepository orderRepository;
 
-  @Scheduled(fixedRate = 120000)
+  @Scheduled(timeUnit = SECONDS, fixedRate = 300)
   @Transactional
   public void processOrders() {
-    LocalDateTime time = LocalDateTime.now().minusSeconds(10);
-
+    LocalDateTime time = LocalDateTime.now().minusMinutes(5);
+    log.info("Checking for orders from status NEW to PROCESSING");
     List<Order> newOrders = orderRepository.findByStatusAndOrderDateBefore(
       OrderStatus.NEW,
       time
     );
 
     for (Order order : newOrders) {
-      order.setStatus(OrderStatus.TESTING);
+      order.setStatus(OrderStatus.PROCESSING);
       orderRepository.save(order);
       log.info("Order {} status automatically changed from NEW to PROCESSING", order.getId());
+    }
+  }
+
+  @Scheduled(timeUnit = SECONDS, fixedRate = 300)
+  @Transactional
+  public void cancelOrders() {
+    LocalDateTime time = LocalDateTime.now().minusMinutes(10);
+    log.info("Checking for orders from status DELIVERED to CANCELLED");
+    List<Order> processingOrders = orderRepository.findByStatusAndOrderDateBefore(
+      OrderStatus.DELIVERED,
+      time
+    );
+
+    for (Order order : processingOrders) {
+      order.setStatus(OrderStatus.CANCELED);
+      orderRepository.save(order);
+      log.info("Order {} status automatically changed from DELIVERED to CANCELLED", order.getId());
+    }
+  }
+
+  @Scheduled(timeUnit = SECONDS, fixedRate = 60)
+  @Transactional
+  public void checkPaidOrders() {
+    LocalDateTime time = LocalDateTime.now().minusMinutes(10);
+    log.info("Checking for orders from status PAID to SHIPPED");
+    List<Order> processingOrders = orderRepository.findByStatusAndOrderDateBefore(
+      OrderStatus.PROCESSING,
+      time
+    );
+
+    for (Order order : processingOrders) {
+      order.setStatus(OrderStatus.SHIPPED);
+      orderRepository.save(order);
+      log.info("Order {} status automatically changed from PAID to SHIPPED", order.getId());
     }
   }
 }
