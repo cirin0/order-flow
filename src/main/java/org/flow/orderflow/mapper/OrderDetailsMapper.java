7509a56fe0c526c9b2aa9ee2458b78794435eb71
Oleksandr <oleksandr.danyliuk.22@pnu.edu.ua>
2025-02-14 03:22:51 +0200

    address from profile

--- src/main/java/org/flow/orderflow/mapper/OrderDetailsMapper.java
+++ src/main/java/org/flow/orderflow/mapper/OrderDetailsMapper.java
@@ -1,4 +1,16 @@
-package org.flow.orderflow.mapper;
-
-public class OrderDetailsMapper {
-}
+//package org.flow.orderflow.mapper;
+//
+//import org.flow.orderflow.dto.order.OrderDetailsDto;
+//import org.flow.orderflow.model.OrderDetails;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//
+//@Mapper(componentModel = "spring")
+//public interface OrderDetailsMapper {
+//  @Mapping(target = "id", source = "id")
+//  OrderDetailsDto toDto(OrderDetails orderDetails);
+//
+//  @Mapping(target = "id", source = "id")
+//  @Mapping(target = "order", ignore = true)
+//  OrderDetails toEntity(OrderDetailsDto dto);
+//}
